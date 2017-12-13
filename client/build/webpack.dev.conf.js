1dbc0b590980831fc39810af69b84fce1e3919ed
leekangbok <haram.lee815@gmail.com>
2017-12-13 18:03:38 +0900

    init

--- client/build/webpack.dev.conf.js
+++ client/build/webpack.dev.conf.js
@@ -35,6 +35,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     quiet: true, // necessary for FriendlyErrorsPlugin
     watchOptions: {
       poll: config.dev.poll,
+      ignored: /node_modules/
     }
   },
   plugins: [
